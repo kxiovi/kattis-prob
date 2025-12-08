03f069ea8d652f40460ddc583a81d89ae69d5acf
Vidhi Patel <vpatel2@ualberta.ca>
2025-12-07 22:44:27 -0700

    start accepted

---
+++ submissions/accepted/reindeersandelves.cpp
@@ -0,0 +1,89 @@
+/*
+  BEGIN-HEADER
+  
+  Name: Vidhi Patel
+  
+  Student-ID: 1702707
+
+  List any resources you used below (eg. urls, name of the algorithm from our code archive).
+  Remember, you are permitted to get help with general concepts about algorithms
+  and problem solving, but you are not permitted to hunt down solutions to
+  these particular problems!
+
+  <List Resources Here>
+  kactl - Dinic
+
+  List any classmate you discussed the problem with. Remember, you can only
+  have high-level verbal discussions. No code should be shared, developed,
+  or even looked at in these chats. No formulas or pseudocode should be
+  written or shared in these chats.
+
+  <List Classmates Here>
+
+  By submitting this code, you are agreeing that you have solved in accordance
+  with the collaboration policy in CMPUT 303/403.
+
+  END-HEADER
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ull = uint64_t;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+struct Dinic {
+	struct Edge {
+		int to, rev;
+		ll c, oc;
+		ll flow() { return max(oc - c, 0LL); } // if you need flows
+	};
+	vi lvl, ptr, q;
+	vector<vector<Edge>> adj;
+	Dinic(int n) : lvl(n), ptr(n), q(n), adj(n) {}
+	void addEdge(int a, int b, ll c, ll rcap = 0) {
+		adj[a].push_back({b, sz(adj[b]), c, c});
+		adj[b].push_back({a, sz(adj[a]) - 1, rcap, rcap});
+	}
+	ll dfs(int v, int t, ll f) {
+		if (v == t || !f) return f;
+		for (int& i = ptr[v]; i < sz(adj[v]); i++) {
+			Edge& e = adj[v][i];
+			if (lvl[e.to] == lvl[v] + 1)
+				if (ll p = dfs(e.to, t, min(f, e.c))) {
+					e.c -= p, adj[e.to][e.rev].c += p;
+					return p;
+				}
+		}
+		return 0;
+	}
+	ll calc(int s, int t) {
+		ll flow = 0; q[0] = s;
+		rep(L,0,31) do { // 'int L=30' maybe faster for random data
+			lvl = ptr = vi(sz(q));
+			int qi = 0, qe = lvl[s] = 1;
+			while (qi < qe && !lvl[t]) {
+				int v = q[qi++];
+				for (Edge e : adj[v])
+					if (!lvl[e.to] && e.c >> (30 - L))
+						q[qe++] = e.to, lvl[e.to] = lvl[v] + 1;
+			}
+			while (ll p = dfs(s, t, LLONG_MAX)) flow += p;
+		} while (lvl[t]);
+		return flow;
+	}
+	bool leftOfMinCut(int a) { return lvl[a] != 0; }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    
+}
